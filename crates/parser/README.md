efa51bc343a85344d768170d17fc922dada5d2dd
hydroperfox <hydroperfox@gmail.com>
2025-01-24 17:48:24 +0000

    Automated report

--- crates/parser/README.md
+++
@@ -1,3 +0,0 @@
-# ActionScript 3 Parser
-
-Refer to the [crate repository](https://github.com/mxmlextrema/as3parser) for full details.
--- crates/parser/tree/invalidated_node.rs
+++
@@ -1,8 +0,0 @@
-use crate::ns::*;
-use serde::{Serialize, Deserialize};
-
-/// Represents a construct that failed to parse.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct InvalidatedNode {
-    pub location: Location,
-}
